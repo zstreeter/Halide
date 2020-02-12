aaf1fb45206c6a37cb4aeb2a29a58a44b71364ef
Andrew Adams <andrew.b.adams@gmail.com>
2020-02-12 12:27:52 -0800

    Handle impure calls that affect bounds in simplify_correlated_differences

--- src/SimplifyCorrelatedDifferences.cpp
+++ src/SimplifyCorrelatedDifferences.cpp
@@ -26,7 +26,12 @@ class SimplifyCorrelatedDifferences : public IRMutator {
 
     Scope<Monotonic> monotonic;
 
-    vector<pair<string, Expr>> lets;
+    struct OuterLet {
+        string name;
+        Expr value;
+        bool may_substitute;
+    };
+    vector<OuterLet> lets;
 
     template<typename LetStmtOrLet, typename StmtOrExpr>
     StmtOrExpr visit_let(const LetStmtOrLet *op) {
@@ -39,23 +44,27 @@ class SimplifyCorrelatedDifferences : public IRMutator {
                 : op(op),
                   binding(scope, op->name, is_monotonic(op->value, loop_var, scope)) {
             }
-            Frame(const LetStmtOrLet *op)
-                : op(op) {
-            }
         };
         std::vector<Frame> frames;
         StmtOrExpr result;
 
+        // Note that we must add *everything* to the monotonic scope
+        // and the list of lets, even things which we can never
+        // substitute in (e.g. impure things). This is for two
+        // reasons. First this pass is used at a time when we still
+        // have nested lets under the same name. If we decide not to
+        // add an inner let, but do add the outer one, then later
+        // references to it will be incorrect. Second, if we don't add
+        // something that happens to be non-monotonic, then
+        // is_monotonic finds a variable that references it in a later
+        // let, it will think it's a constant, not an unknown.
         do {
             result = op->body;
-            if (op->value.type() == Int(32) && is_pure(op->value)) {
-                frames.emplace_back(op, loop_var, monotonic);
-                Expr new_value = mutate(op->value);
-                lets.emplace_back(op->name, new_value);
-                frames.back().new_value = std::move(new_value);
-            } else {
-                frames.emplace_back(op);
-            }
+            frames.emplace_back(op, loop_var, monotonic);
+            Expr new_value = mutate(op->value);
+            bool may_substitute_in = new_value.type() == Int(32) && is_pure(new_value);
+            lets.emplace_back(OuterLet{op->name, new_value, may_substitute_in});
+            frames.back().new_value = std::move(new_value);
         } while ((op = result.template as<LetStmtOrLet>()));
 
         result = mutate(result);
@@ -66,9 +75,7 @@ class SimplifyCorrelatedDifferences : public IRMutator {
             } else {
                 result = LetStmtOrLet::make(it->op->name, it->op->value, result);
             }
-            if (it->binding.bound()) {
-                lets.pop_back();
-            }
+            lets.pop_back();
         }
 
         return result;
@@ -166,7 +173,17 @@ class SimplifyCorrelatedDifferences : public IRMutator {
             (mb == Monotonic::Unknown && ma != Monotonic::Constant)) {
 
             for (auto it = lets.rbegin(); it != lets.rend(); it++) {
-                e = Let::make(it->first, it->second, e);
+                if (expr_uses_var(e, it->name)) {
+                    if (!it->may_substitute) {
+                        // We have to stop here. Can't continue
+                        // because there might be an outer let with
+                        // the same name that we *can* substitute in,
+                        // and then inner uses will get the wrong
+                        // value.
+                        break;
+                    }
+                }
+                e = Let::make(it->name, it->value, e);
             }
             e = common_subexpression_elimination(e);
             e = solve_expression(e, loop_var).result;
