ebde8ee9ce0deae82c5277c31a02e6b14452a5ac
Alex Reinking <alex_reinking@berkeley.edu>
2020-02-17 14:51:25 -0800

    Chop down top level CMakeLists.txt

--- cmake/HalideTestHelpers.cmake
+++ cmake/HalideTestHelpers.cmake
@@ -19,3 +19,11 @@ function(add_halide_test TARGET)
         set_tests_properties(${TARGET} PROPERTIES WILL_FAIL true)
     endif ()
 endfunction()
+
+function(halide_project name folder)
+    add_executable("${name}" ${ARGN})
+    target_link_libraries("${name}" PRIVATE Halide::Halide ${CMAKE_DL_LIBS} Threads::Threads $<$<CXX_COMPILER_ID:MSVC>:Kernel32>)
+    set_target_properties("${name}" PROPERTIES
+                          FOLDER "${folder}"
+                          ENABLE_EXPORTS True)
+endfunction(halide_project)
